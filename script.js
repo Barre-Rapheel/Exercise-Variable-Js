90b5c554e3dac4d27029e6c4f683ae84335e4c31
Raphaël <barre.raphael97@gmail.com>
2024-10-11 10:59:29 +0200

    Excercie-variable-js

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+    const name = "David";  
+    const lastname = "Bruno"; 
+  
+    
+    let DavidBiography = "Hi, I'm David Bruno from SF, I like to cook and meet new people."; 
+  
+  
+    let profilePicture = "http://www.go.com/davif.png"; 
+  
+    let city = "San Francisco"; 
+  
+    let followers = 109; 
+    let following = 200; 
+
+   
